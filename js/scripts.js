3274417550e40ac3b3ec8e5862685ee4891c1133
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-06-01 15:37:04 -0700

    add function to highlight p
    Co-authored-by: Garrett Broughton <gbrough@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('#p1').click(function() {
+    $('#p1').removeClass('p-class');
+    $('#p1').addClass('p-class');
+  });
+  $('#p2').click(function() {
+    $('#p2').addClass('p-class');
+  });
+  $('#p3').click(function() {
+    $('#p3').addClass('p-class');
+  });
+});
